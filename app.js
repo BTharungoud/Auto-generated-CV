16a65add160c59b0f0753adf3e6cabf597f5bd43
BTharungoud <122255721+BTharungoud@users.noreply.github.com>
2023-02-05 00:46:08 +0530

    added

--- app.js
+++ app.js
@@ -30,25 +30,25 @@ function handleSubmit(e) {
   // const name_resume = document.getElementById("name_resume");
   // name_resume.innerHTML = fName.value;
    document.getElementById("name_resume").innerHTML=fName.value;
-   document.getElementById("fName").value='';
+   //document.getElementById("fName").value='';
    document.getElementById("email_resume").innerHTML=email.value;
-   document.getElementById("email").value='';
+   //document.getElementById("email").value='';
    const phone_resume = document.getElementById("phone_resume");
    phone_resume.textContent="| ";
    phone_resume.textContent+=phone.value;
-   document.getElementById("phone").value='';
+   //document.getElementById("phone").value='';
    const github_resume=document.getElementById("github_resume");
    github_resume.textContent="| ";
    github_resume.textContent+=github.value;
-   document.getElementById("github").value='';
+   //document.getElementById("github").value='';
    const linkedin_resume=document.getElementById("linkedin_resume");
    linkedin_resume.textContent="| ";
    linkedin_resume.textContent+=linkedin.value;
-   document.getElementById("linkedin").value='';
+   //document.getElementById("linkedin").value='';
    document.getElementById("college_name").innerHTML=College.value;
-   document.getElementById("College").value='';
+   //document.getElementById("College").value='';
    document.getElementById("degree_resume").innerHTML=Degree.value;
-   document.getElementById("Degree").value='';
+   //document.getElementById("Degree").value='';
    document.getElementById("startingyear_resume").innerHTML=Startingyear.value;
    //document.getElementById("Startingyear").value='';
    const passingyear_resume = document.getElementById("passingyear_resume");
@@ -58,37 +58,45 @@ function handleSubmit(e) {
    const cgpa_resume=document.getElementById("cgpa_resume");
    cgpa_resume.textContent="CGPA:"
    cgpa_resume.textContent+=CGPA.value;
-   document.getElementById("CGPA").value='';
+   //document.getElementById("CGPA").value='';
    document.getElementById("skills_resume").innerHTML=Skills.value;
-   document.getElementById("Skills").value='';
+   //document.getElementById("Skills").value='';
    document.getElementById("project_resume").innerHTML=Project.value;
-   document.getElementById("Project").value='';
+  // document.getElementById("Project").value='';
    document.getElementById("projectdescription_resume").innerHTML=p_description.value;
-   document.getElementById("p_description").value='';
+   //document.getElementById("p_description").value='';
    const link_resume=document.getElementById("link_resume");
    link_resume.textContent="Link-";
    link_resume.textContent+=Link.value;
-   document.getElementById("Link").value='';
+  // document.getElementById("Link").value='';
    document.getElementById("designation_resume").innerHTML=Designation.value;
-   document.getElementById("Designation").value='';
+   //document.getElementById("Designation").value='';
    document.getElementById("company_resume").innerHTML=Company.value;
-   document.getElementById("Company").value='';
+  // document.getElementById("Company").value='';
    document.getElementById("duration_resume").innerHTML=Duration.value;
-   document.getElementById("Duration").value='';
+   //document.getElementById("Duration").value='';
    document.getElementById("experiencedescription_resume").innerHTML=e_description.value;
-   document.getElementById("e_description").value='';
+   //document.getElementById("e_description").value='';
   //   similarly for all the other fields
 }
 
 function handleLeft() {
   const left = document.querySelector(".left");
+  left.style.display="none";
   const right = document.querySelector(".right");
+  right.style.width="100%";
   const print = document.querySelector(".print");
+  print.style.display="block";
   // write more codes here
+
 }
 function handlePrint() {
   const right = document.querySelector(".right");
+  right.style.width="70%";
   const left = document.querySelector(".left");
+  left.style.display="block";
   const print = document.querySelector(".print");
+  print.style.display="none";
+  
   // write more codes here
 }
